b6cb5f3d78d6e30c042df5ffe0775e523b93fdcd
VanChayPho <vanntph24290@fpt.edu.vn>
2023-10-21 15:22:40 +0700

    duoi mk

---
+++ WEBSITE_BAN_QUAN_AO_NAM/src/main/java/com/poly/datn/controller/user/ForGotPasswort.java
@@ -0,0 +1,24 @@
+package com.poly.datn.controller.user;
+
+import com.poly.datn.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class ForGotPasswort {
+
+    @Autowired
+    private User user;
+    @GetMapping("/forgot-passwort/user")
+    public String getUserName(@AuthenticationPrincipal Principal principal){
+        if (principal == null) {
+            return "redirect:/forgot-passwort";
+        }
+        return "User/forgot-password/user";
+    }
+
+}
