9796ed8901d8cfd1b853acd452e1f756a25d8063
hungh <hunglvph28525@gmail.com>
2024-01-15 16:09:47 +0700

    ok

--- WEBSITE_BAN_QUAN_AO_NAM/src/main/java/com/poly/datn/config/UpdateJopStatus.java
+++ WEBSITE_BAN_QUAN_AO_NAM/src/main/java/com/poly/datn/config/UpdateJopStatus.java
@@ -17,19 +17,19 @@ public class UpdateJopStatus {
     @Autowired
     private InvoiceService invoiceService;
 
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 30000)
     public void schedulePromotionProgramUpdate() {
         System.out.println("đã update");
         service.jobUpdate();
     }
 
-    @Scheduled(cron = "0 0 23 * * *")
-    public void scheduleInvoiceProgramUpdate() {
-        LocalDate currentDate = LocalDate.now();
-        LocalTime currentTime = LocalTime.now();
-        if (currentTime.getHour() == 23 && currentTime.getMinute() == 0 && currentTime.getSecond() == 0) {
-            System.out.println("đã xóa khi hết ngày");
-            invoiceService.deleteInvoice();
-        }
-    }
+//    @Scheduled(cron = "0 0 23 * * *")
+//    public void scheduleInvoiceProgramUpdate() {
+//        LocalDate currentDate = LocalDate.now();
+//        LocalTime curre ntTime = LocalTime.now();
+//        if (currentTime.getHour() == 23 && currentTime.getMinute() == 0 && currentTime.getSecond() == 0) {
+//            System.out.println("đã xóa khi hết ngày");
+//            invoiceService.deleteInvoice();
+//        }
+//    }
 }
